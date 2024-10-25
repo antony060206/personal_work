24e30b8511fa687149b428450d0187cc939fbaa6
Antony <anthony060206@icloud.com>
2024-10-25 02:07:29 -0600

    assignmetn 2

--- School_work/Comput 274/assignment2/Question1/solutionQuestion1.py
+++ School_work/Comput 274/assignment2/Question1/solutionQuestion1.py
@@ -14,21 +14,21 @@ def fractal_eval(expr):
     expr_string = expr_string[:sqrleft] + " " + expr_string[sqrleft+1:]
     expr_string = expr_string.replace(expr_string[sqrright], " ", 1)
 
-    calculate_str = expr_string[sqrleft + 1:sqrright]
-    expr_string = expr_string.replace(expr_string[sqrleft + 1:sqrright], str((eval(calculate_str)** 2)))
+    calculate_str = str((eval(expr_string[sqrleft + 1:sqrright]))**2)
+    expr_string_left = expr_string[:sqrleft]
+    expr_string_right = expr_string[sqrright:]
+    expr_string = expr_string_left + calculate_str + expr_string_right
 
-    if expr_string.count("[") >=  1:
 
-        fractal_eval(expr_string)
-
+    if expr_string.count("[") >= 1 :
+        return fractal_eval(expr_string)
     else:
-        print(expr_string.strip())
-        return int(expr_string)
+        return eval(expr_string)
 
-                
 def main():
+
     exp = input()
-    print((fractal_eval(exp)))
+    print(round(fractal_eval(exp)))
 
 if __name__ == "__main__":
     main()
--- School_work/Comput 274/assignment2/Question2/solutionQuestion2.py
+++ School_work/Comput 274/assignment2/Question2/solutionQuestion2.py
@@ -1,12 +1,45 @@
-# Name: <replace with your name>
-# ccid: <replace with your ccid>
-# studentId: <replace with your student id>
-# operating system: <replace with yours>
-# python version: <replace with your python version>
+# Name: Antony Feng
+# ccid: zfeng8
+# studentId: 1802829
+# operating system: windows 10
+# python version: 3.12
 
 def simplify_path(path):
-    # Add your implementation here
-    pass
+
+    if not path.startswith('/') and len(path) >= 1 and len(path) <= 100:
+        return "Invalid Path"
+
+    else:
+        groups = []
+        current = []
+        path_length = 0
+
+        for char in path:
+            path_length += 1
+
+            if char == '/':
+                if len(current) != 0:
+                    parts = ''.join(current)
+                    if parts == "..":
+                        if len(groups) != 0:
+                            groups.pop()
+                    elif parts != ".":
+                        groups.append(parts)
+                    current.clear()
+            else:
+                current.append(char)
+
+        if len(current) != 0:
+                parts = ''.join(current)
+                if parts == "..":
+                    groups.pop()
+                elif parts != ".":
+                    groups.append(parts)
+
+    if path_length == len(path):
+        return "/" + "/".join(groups)
+    else:
+        return "Invalid Path"
     
 def main():
     # Takes Unix path as input
