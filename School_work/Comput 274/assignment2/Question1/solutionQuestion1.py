3d9bb2d887820ed378db1edfe17dce4b482d64f9
antony060206 <70788705+antony060206@users.noreply.github.com>
2024-10-24 18:25:18 -0600

    assignment 2

--- School_work/Comput 274/assignment2/Question1/solutionQuestion1.py
+++ School_work/Comput 274/assignment2/Question1/solutionQuestion1.py
@@ -1,11 +1,29 @@
-# Name: <replace with your name>
-# ccid: <replace with your ccid>
-# studentId: <replace with your student id>
-# operating system: <replace with yours>
-# python version: <replace with your python version>
+# Name: Antony Feng
+# ccid: zfeng8
+# studentId: 1802829
+# operating system: windows 10
+# python version: 3.12
 
 def fractal_eval(expr):
-    pass
+    expr_string = str(expr)
+
+    sqrleft = expr_string.rfind("[")
+    sqrright = expr_string.find("]")
+
+
+    expr_string = expr_string[:sqrleft] + " " + expr_string[sqrleft+1:]
+    expr_string = expr_string.replace(expr_string[sqrright], " ", 1)
+
+    calculate_str = expr_string[sqrleft + 1:sqrright]
+    expr_string = expr_string.replace(expr_string[sqrleft + 1:sqrright], str((eval(calculate_str)** 2)))
+
+    if expr_string.count("[") >=  1:
+
+        fractal_eval(expr_string)
+
+
+    return eval(expr_string)
+
                 
 def main():
     exp = input()
